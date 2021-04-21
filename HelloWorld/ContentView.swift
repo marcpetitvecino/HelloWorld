908f00a437e623e0cea2e5a238179bf814413eee
marcpetitvecino <marc.petit@mataro.epiaedu.cat>
2021-04-22 00:16:57 +0200

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Marc Petit Vecino on 22/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Marc Petit Vecino on 22/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
